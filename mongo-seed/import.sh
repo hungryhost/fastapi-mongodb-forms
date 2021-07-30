cc241eaa87dab66456fe97eb8906a227d50bde77
Юрий Бородин <yuiborodin@miem.hse.ru>
2021-07-30 11:59:12 +0300

    fixed mongo config

--- mongo-seed/import.sh
+++ mongo-seed/import.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-mongoimport --host db --db forms --collection form_collection --type json --file /mongo-seed/forms_collection.json --jsonArray
+mongoimport --host mongodb --db forms --collection form_collection --type json --file /mongo-seed/forms_collection.json --jsonArray
